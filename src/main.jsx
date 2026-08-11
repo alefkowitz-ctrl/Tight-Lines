9895193b3e53584b90c1bbc710d2fe9e16e19b26
alefkowitz-ctrl <amlefkowitz@gmail.com>
2026-08-11 00:53:46 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import Root from "./App.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Root />
+    <Analytics />
   </StrictMode>
 );
 
